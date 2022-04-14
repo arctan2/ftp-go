ac81570b83a678791857f464aad29cb66a558c4a
ProClasic <prateekp.0055@gmail.com>
2022-04-14 20:19:10 +0530

    abort controlled files fetch

--- server/http-server/public/script.js
+++ server/http-server/public/script.js
@@ -1,3 +1,5 @@
+const abortCtlr = { controller: null, isActive: false };
+
 const vueApp = new Vue({
   el: '#app',
   data: { 
@@ -8,39 +10,52 @@ const vueApp = new Vue({
     loading: false
   },
   methods: {
-    async getFilesFromServer(path) {
-      if(this.loading) return
+    async getFilesFromServer() {
+      if(abortCtlr.isActive) abortCtlr.controller?.abort();
       this.loading = true
       if(this.errMsg !== "") {
         this.errMsg = ""
         this.files = []
       }
 
-      const res = await fetch("/ls", {
-        headers: { "Content-Type": "text/json" },
-        method: "POST",
-        body: JSON.stringify({ path: path + "/" })
-      })
-      const data = await res.json()
-      
-      if(data && data.err) {
-        this.curDir = path
-        this.errMsg = data.errMsg
-      } else if(res.ok) {
-        this.files = (data.files === null) ? [] : data.files
-        this.curDir = path
-      } else this.errMsg = "something went wrong."
-      if(this.curDir === "" && this.dirType === "unix") this.curDir = "/"
-      this.loading = false
+      abortCtlr.controller = new AbortController();
+      abortCtlr.isActive = true;
+
+      try {
+        const res = await fetch("/ls", {
+          headers: { "Content-Type": "text/json" },
+          method: "POST",
+          body: JSON.stringify({ path: this.curDir + "/" }),
+          signal: abortCtlr.controller.signal
+        })
+        const data = await res.json()
+        
+        if(data && data.err) {
+          this.errMsg = data.errMsg
+        } else if(res.ok) {
+          this.files = (data.files === null) ? [] : data.files
+        } else this.errMsg = "something went wrong."
+        abortCtlr.controller = null;
+        abortCtlr.isActive = false;
+        this.loading = false
+      } catch(err) {
+        console.log(err)
+      }
     },
     async cd(dirName) {
       if(dirName === "..") {
         let lastIdx = this.curDir.lastIndexOf("/")
-        if(lastIdx !== -1)
-          await this.getFilesFromServer(this.curDir.slice(0, lastIdx))
+        if(lastIdx !== -1) {
+          this.curDir = this.curDir.slice(0, lastIdx)
+          if(this.curDir === "" && this.dirType === "unix") this.curDir = "/"
+          await this.getFilesFromServer()
+        }
       }
-      else 
-        await this.getFilesFromServer(this.curDir + "/" + dirName)
+      else {
+        this.curDir = this.curDir + "/" + dirName
+        if(this.curDir === "" && this.dirType === "unix") this.curDir = "/"
+        await this.getFilesFromServer()
+      }
     },
     async getDirFromServer() {
       const res = await fetch("/pwd", { method: "GET", headers: { "Content-Type": "text/json" } })
